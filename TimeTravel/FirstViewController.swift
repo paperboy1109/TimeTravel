0f97218e1583cb6981970793c0a97a2454cbb810
Daniel J Janiak <danieljjaniak@icloud.com>
2016-11-20 13:19:05 -0800

    Initial Commit

---
+++ TimeTravel/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TimeTravel
+//
+//  Created by Daniel J Janiak on 11/20/16.
+//  Copyright © 2016 redwoodempiredev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

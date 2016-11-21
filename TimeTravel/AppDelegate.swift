aca112a5dbcba6235d7e28ba4973125486ff04af
Daniel J Janiak <danieljjaniak@icloud.com>
2016-11-20 16:29:16 -0800

    feat: Add custom font (PT Sans)

--- TimeTravel/AppDelegate.swift
+++ TimeTravel/AppDelegate.swift
@@ -15,7 +15,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
+        
+        /* Get a list of the available fonts */
+        
+        for family: String in UIFont.familyNames {
+            
+            print("\(family)")
+            
+            for names: String in UIFont.fontNames(forFamilyName: family) {
+                print("** \(names) ***")
+            }
+        }
+        
+                
         return true
     }
 
